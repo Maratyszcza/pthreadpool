6673a4c71fe35e077c6843a74017d9c25610c537
Marat Dukhan <marat@fb.com>
2018-11-21 14:52:31 -0800

    Update ndk-build configs

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,25 +1,17 @@
 LOCAL_PATH := $(call my-dir)/..
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := gtest
-LOCAL_SRC_FILES := $(LOCAL_PATH)/deps/googletest/googletest/src/gtest-all.cc
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/deps/googletest/googletest/include
-LOCAL_C_INCLUDES := $(LOCAL_EXPORT_C_INCLUDES) $(LOCAL_PATH)/deps/googletest/googletest
+LOCAL_MODULE := pthreadpool_interface
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := pthreadpool
-LOCAL_SRC_FILES := $(LOCAL_PATH)/src/threadpool-pthreads.c
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_C_INCLUDES := $(LOCAL_EXPORT_C_INCLUDES) $(LOCAL_PATH)/deps/fxdiv/include
+LOCAL_SRC_FILES := src/threadpool-pthreads.c
+LOCAL_CFLAGS := -std=c99 -Wall
+LOCAL_STATIC_LIBRARIES := pthreadpool_interface fxdiv
 include $(BUILD_STATIC_LIBRARY)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := pthreadpool-test
-LOCAL_SRC_FILES := $(LOCAL_PATH)/test/pthreadpool.cc
-LOCAL_C_INCLUDES := 
-LOCAL_CFLAGS := -D__STDC_CONSTANT_MACROS=1
-LOCAL_LDLIBS := -latomic
-LOCAL_STATIC_LIBRARIES := pthreadpool gtest
-include $(BUILD_EXECUTABLE)
+$(call import-add-path,$(LOCAL_PATH)/deps)
 
+$(call import-module,fxdiv/jni)
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,3 +1,4 @@
-APP_PLATFORM := android-14
+APP_PLATFORM := android-15
 APP_PIE := true
 APP_STL := c++_static
+APP_ABI := all
