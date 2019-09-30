51d469480dde41c539172c1cdba851df762460a8
Marat Dukhan <maratek@google.com>
2019-09-30 16:21:19 -0700

    Fix configure.py

--- configure.py
+++ configure.py
@@ -14,9 +14,9 @@ def main(args):
     with build.options(source_dir="src", extra_include_dirs="src", deps=build.deps.fxdiv):
         sources = ["threadpool-legacy.c"]
         if build.target.is_emscripten:
-            source.append("threadpool-shim.c")
+            sources.append("threadpool-shim.c")
         else:
-            source.append("threadpool-pthreads.c")
+            sources.append("threadpool-pthreads.c")
         build.static_library("pthreadpool", [build.cc(src) for src in sources])
 
     with build.options(source_dir="test", deps=[build, build.deps.googletest]):
