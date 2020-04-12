bfa3b9ce6cb71dc8b792e39d24717320a4f92572
Marat Dukhan <maratek@gmail.com>
2020-04-12 13:18:52 -0700

    Minimally fix confu build

--- configure.py
+++ configure.py
@@ -12,11 +12,15 @@ def main(args):
     build.export_cpath("include", ["pthreadpool.h"])
 
     with build.options(source_dir="src", extra_include_dirs="src", deps=build.deps.fxdiv):
-        sources = ["threadpool-legacy.c"]
+        sources = ["legacy-api.c", "portable-api.c"]
         if build.target.is_emscripten:
-            sources.append("threadpool-shim.c")
+            sources.append("shim.c")
+        elif build.target.is_macos:
+            sources.append("gcd.c")
+        elif build.target.is_windows:
+            sources.append("windows.c")
         else:
-            sources.append("threadpool-pthreads.c")
+            sources.append("pthreads.c")
         build.static_library("pthreadpool", [build.cc(src) for src in sources])
 
     with build.options(source_dir="test", deps=[build, build.deps.googletest]):
