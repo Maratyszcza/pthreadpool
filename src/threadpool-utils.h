b28dc145d14da4f336a9f7fa98b4d323a3fa7d02
Marat Dukhan <maratek@google.com>
2022-11-17 21:45:13 -0800

    Support ARM64EC ABI

--- src/threadpool-utils.h
+++ src/threadpool-utils.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 /* SSE-specific headers */
-#if defined(__SSE__) || defined(__x86_64__) || defined(_M_X64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
+#if defined(__SSE__) || defined(__x86_64__) || defined(_M_X64) && !defined(_M_ARM64EC) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
 	#include <xmmintrin.h>
 #endif
 
@@ -15,12 +15,12 @@
 
 
 struct fpu_state {
-#if defined(__SSE__) || defined(__x86_64__) || defined(_M_X64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
-	uint32_t mxcsr;
-#elif defined(__GNUC__) && defined(__arm__) && defined(__ARM_FP) && (__ARM_FP != 0) || defined(_MSC_VER) && defined(_M_ARM)
+#if defined(__GNUC__) && defined(__arm__) && defined(__ARM_FP) && (__ARM_FP != 0) || defined(_MSC_VER) && defined(_M_ARM)
 	uint32_t fpscr;
-#elif defined(__GNUC__) && defined(__aarch64__) || defined(_MSC_VER) && defined(_M_ARM64)
+#elif defined(__GNUC__) && defined(__aarch64__) || defined(_MSC_VER) && (defined(_M_ARM64) || defined(_M_ARM64EC))
 	uint64_t fpcr;
+#elif defined(__SSE__) || defined(__x86_64__) || defined(_M_X64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
+	uint32_t mxcsr;
 #else
 	char unused;
 #endif
@@ -28,12 +28,12 @@ struct fpu_state {
 
 static inline struct fpu_state get_fpu_state() {
 	struct fpu_state state = { 0 };
-#if defined(__SSE__) || defined(__x86_64__) || defined(_M_X64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
-	state.mxcsr = (uint32_t) _mm_getcsr();
-#elif defined(_MSC_VER) && defined(_M_ARM)
+#if defined(_MSC_VER) && defined(_M_ARM)
 	state.fpscr = (uint32_t) _MoveFromCoprocessor(10, 7, 1, 0, 0);
-#elif defined(_MSC_VER) && defined(_M_ARM64)
+#elif defined(_MSC_VER) && (defined(_M_ARM64) || defined(_M_ARM64EC))
 	state.fpcr = (uint64_t) _ReadStatusReg(0x5A20);
+#elif defined(__SSE__) || defined(__x86_64__) || defined(_M_X64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
+	state.mxcsr = (uint32_t) _mm_getcsr();
 #elif defined(__GNUC__) && defined(__arm__) && defined(__ARM_FP) && (__ARM_FP != 0)
 	__asm__ __volatile__("VMRS %[fpscr], fpscr" : [fpscr] "=r" (state.fpscr));
 #elif defined(__GNUC__) && defined(__aarch64__)
@@ -43,27 +43,25 @@ static inline struct fpu_state get_fpu_state() {
 }
 
 static inline void set_fpu_state(const struct fpu_state state) {
-#if defined(__SSE__) || defined(__x86_64__) || defined(_M_X64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
-	_mm_setcsr((unsigned int) state.mxcsr);
-#elif defined(_MSC_VER) && defined(_M_ARM)
+#if defined(_MSC_VER) && defined(_M_ARM)
 	_MoveToCoprocessor((int) state.fpscr, 10, 7, 1, 0, 0);
-#elif defined(_MSC_VER) && defined(_M_ARM64)
+#elif defined(_MSC_VER) && (defined(_M_ARM64) || defined(_M_ARM64EC))
 	_WriteStatusReg(0x5A20, (__int64) state.fpcr);
 #elif defined(__GNUC__) && defined(__arm__) && defined(__ARM_FP) && (__ARM_FP != 0)
 	__asm__ __volatile__("VMSR fpscr, %[fpscr]" : : [fpscr] "r" (state.fpscr));
 #elif defined(__GNUC__) && defined(__aarch64__)
 	__asm__ __volatile__("MSR fpcr, %[fpcr]" : : [fpcr] "r" (state.fpcr));
+#elif defined(__SSE__) || defined(__x86_64__) || defined(_M_X64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
+	_mm_setcsr((unsigned int) state.mxcsr);
 #endif
 }
 
 static inline void disable_fpu_denormals() {
-#if defined(__SSE__) || defined(__x86_64__) || defined(_M_X64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
-	_mm_setcsr(_mm_getcsr() | 0x8040);
-#elif defined(_MSC_VER) && defined(_M_ARM)
+#if defined(_MSC_VER) && defined(_M_ARM)
 	int fpscr = _MoveFromCoprocessor(10, 7, 1, 0, 0);
 	fpscr |= 0x1000000;
 	_MoveToCoprocessor(fpscr, 10, 7, 1, 0, 0);
-#elif defined(_MSC_VER) && defined(_M_ARM64)
+#elif defined(_MSC_VER) && (defined(_M_ARM64) || defined(_M_ARM64EC))
 	__int64 fpcr = _ReadStatusReg(0x5A20);
 	fpcr |= 0x1080000;
 	_WriteStatusReg(0x5A20, fpcr);
@@ -92,6 +90,8 @@ static inline void disable_fpu_denormals() {
 			"ORR %w[fpcr], %w[fpcr], 0x80000\n"
 			"MSR fpcr, %[fpcr]\n"
 		: [fpcr] "=r" (fpcr));
+#elif defined(__SSE__) || defined(__x86_64__) || defined(_M_X64) || (defined(_M_IX86_FP) && _M_IX86_FP >= 1)
+	_mm_setcsr(_mm_getcsr() | 0x8040);
 #endif
 }
 
